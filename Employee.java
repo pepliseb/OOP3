b33947d21a92f2e77e80ed6de5e20f9cc62f46cb
eman <eman@onet.eu>
2017-07-29 22:01:15 +0200

    start project

---
+++ Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+
+    private String company;
+    private String name;
+    private String id;
+
+
+    public Employee(String company, String name, String id) {
+        this.company = company;
+        this.name = name;
+        this.id = id;
+    }
+}
