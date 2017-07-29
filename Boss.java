823e8dafc3db99b76b1916ec9d4ab53a6a01a679
eman <eman@onet.eu>
2017-07-29 23:25:04 +0200

    Nowy konstruktor w Boss i nowe pole isHappy

--- Boss.java
+++ Boss.java
@@ -3,10 +3,24 @@
 
 
 public class Boss extends Employee {
+    private boolean isHappy;
+
     public Boss(String name, String id, int salary) {
         super(name, id, salary );
+    }
 
 
+    public Boss(String name, String id, int salary, boolean isHappy) {
+        super(name, id, salary );
+        this.isHappy = isHappy;
+    }
+
+    public boolean isHappy() {
+        return isHappy;
+    }
+
+    public void setHappy(boolean happy) {
+        isHappy = happy;
     }
 
     @Override
