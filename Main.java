aa43963c3826e3397822f3110f55001f14f8d5b9
eman <eman@onet.eu>
2017-07-29 22:45:53 +0200

    poprawa kodu

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Boss oskar = new Boss("Oskar", "123444", 4566);
-        Boss seb = new Boss("Seb", "123443334", 455);
+
         Worker kuba = new Worker("Kuba","222222", 1200);
-        Lecturer irek = new Lecturer("Irek","11111111", 1222);
         kuba.getVoice();
+
+
+        Boss oskar = new Boss("Oskar", "123444", 4566);
         oskar.getVoice();
+
+        Lecturer irek = new Lecturer("Irek","11111111", 1222);
         irek.getVoice();
 
 
